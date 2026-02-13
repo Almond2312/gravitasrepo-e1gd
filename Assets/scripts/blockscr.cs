c94807c7ebd0cfe5f891723bd9eaadf1378d5bba
Almond2312 <aystromberg@gmail.com>
2026-02-13 09:23:40 -0800

    friction
    pushblock now has friction when being moved but slides on walls smoothly. also unknown file changes?

--- Assets/scripts/blockscr.cs
+++ Assets/scripts/blockscr.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class blockscr : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private int Touch = 0;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag("ground"))
+        {
+            return;
+        }
+        Touch++;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Touch--;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Touch > 0)
+        {
+                rb.linearVelocity = Vector2.zero;
+        }
     }
 }
