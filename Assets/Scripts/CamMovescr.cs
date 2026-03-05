a32ae68bf905e0961a0ab79a99eefe1ea5f7de0b
Almond2312 <aystromberg@gmail.com>
2026-03-05 15:56:33 -0800

    long slope and fixed screen ratio

--- Assets/Scripts/CamMovescr.cs
+++ Assets/Scripts/CamMovescr.cs
@@ -10,9 +10,10 @@ public class CamMovescr : MonoBehaviour
     [SerializeField] float time = .4f;
     private float size;
     Camera c;
+    [SerializeField] float targetAspect = 16f / 9f;
     public void moveCam(Transform T, float s)
     {
-        Debug.Log("in movecam");
+        //Debug.Log("in movecam");
         end = T;
         size = s;
         moving = true;
@@ -20,6 +21,36 @@ public class CamMovescr : MonoBehaviour
     void Awake()
     {
         moving = false;
+    }
+    void SetAspectRatio()
+    {
+        float windowAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = c.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            c.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+
+            Rect rect = c.rect;
+
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+
+            c.rect = rect;
+        }
     }
     private void Start()
     {
